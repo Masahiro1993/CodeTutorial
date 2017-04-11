e6560bde50293cbd4b5ecba797de373807c0e2d9
Masahiro1993 <mikesh_nemkul@hotmail.com>
2017-04-11 16:09:37 +0545

    prob with grid view

--- app/src/main/java/com/mikesh/hp/codetutorial/MainActivity.java
+++ app/src/main/java/com/mikesh/hp/codetutorial/MainActivity.java
@@ -114,17 +114,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_course) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_mreport) {
 
         } else if (id == R.id.nav_slideshow) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_settings) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_ta) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_attendance) {
 
         }
 
